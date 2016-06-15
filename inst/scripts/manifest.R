dcf795f4f14f14cfecb297e3ccbe64f14f0b83f7
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2016-06-15 15:19:59 -0400

    updated version

--- inst/scripts/manifest.R
+++ inst/scripts/manifest.R
@@ -1,34 +1,36 @@
 library(minfi)
 
 ## Needs to be uncompressed
-manifestFile <- "../../../data_files/manifest_epic_v10b2/MethylationEPIC_v-1-0_B2.csv"
-stopifnot(file.exists(manifestFile))
+manifestFile <- "../../../IlluminaHumanMethylationEPIC_files/data/MethylationEPIC_v-1-0_B2.csv"
+if(!file.exists(manifestFile)) {
+    cat("Missing files, quitting\n")
+    q(save = "no")
+}
 maniTmp <- minfi:::read.manifest.EPIC(manifestFile)
 
 ## Checking
-manifest <- maniTmp$manifest
-address.all <- c(manifest$AddressA, manifest$AddressB)
-sum(address.all == "")
-sum(is.na(address.all))
-address.all <- address.all[address.all != ""]
-length(address.all)
-stopifnot(!anyDuplicated(address.all))
-library(illuminaio)
-epic <- readIDAT("../../../data_files/Demo_Data_EPIC/200144450018/200144450018_R04C01_Grn.idat")
-address.epic <- as.character(epic$MidBlock)
-sum(!address.epic %in% address.all) ## Set of addresses in the IDAT file not part of the manifest
-sum(! address.all %in% address.epic) ## set of addresses not in IDAT file.
-nrow(manifest)
-any(manifest$AddressA != "" & !manifest$AddressA %in% address.epic)
-any(manifest$AddressB != "" & !manifest$AddressB %in% address.epic)
-wh <- which(manifest$AddressB != "" & !manifest$AddressB %in% address.epic)
-tmp <- manifest[wh,]
-table(tmp$Infinium_Design_Type)
-table(tmp$Color_Channel)
-table(tmp$Methyl450_Loci)
-
-## Controls ok
-all(maniTmp$controls[,1] %in% address.epic)
+## manifest <- maniTmp$manifest
+## address.all <- c(manifest$AddressA, manifest$AddressB)
+## sum(address.all == "")
+## sum(is.na(address.all))
+## address.all <- address.all[address.all != ""]
+## length(address.all)
+## stopifnot(!anyDuplicated(address.all))
+## library(illuminaio)
+## epic <- readIDAT("../../../data_files/Demo_Data_EPIC/200144450018/200144450018_R04C01_Grn.idat")
+## address.epic <- as.character(epic$MidBlock)
+## sum(!address.epic %in% address.all) ## Set of addresses in the IDAT file not part of the manifest
+## sum(! address.all %in% address.epic) ## set of addresses not in IDAT file.
+## nrow(manifest)
+## any(manifest$AddressA != "" & !manifest$AddressA %in% address.epic)
+## any(manifest$AddressB != "" & !manifest$AddressB %in% address.epic)
+## wh <- which(manifest$AddressB != "" & !manifest$AddressB %in% address.epic)
+## tmp <- manifest[wh,]
+## table(tmp$Infinium_Design_Type)
+## table(tmp$Color_Channel)
+## table(tmp$Methyl450_Loci)
+## ## Controls ok
+## all(maniTmp$controls[,1] %in% address.epic)
 
 ## Manifest package
 maniList <- maniTmp$manifestList
@@ -48,3 +50,6 @@ save(IlluminaHumanMethylationEPICmanifest, compress = "xz",
      file = "../../data/IlluminaHumanMethylationEPICmanifest.rda")
 sessionInfo()
 rm(list = ls())
+
+sessionInfo()
+q(save = "no")
